d14613dae7e68a0f952cc96496e9a5962dba88b9
Jormaedes Luís <jormahed@gmail.com>
2025-11-13 18:40:31 +0100

    Adicionei regras para as cores alternarem

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-//const squares = [...document.querySelectorAll('.square')];
 const btnReset = document.querySelector('#reset-btn');
 const btnChangeGridSize = document.querySelector('#change-grid-size');
 const gridContainer = document.querySelector('#grid-container');
+const btnRainbow = document.querySelector('#rainbow');
 const SIZEGRID = 500;
-let numberOfSquares;
+let numberOfSquares = '';
+let defaultColor = 1;
 
 btnReset.addEventListener('click', ()=>{
 	const squares = [...document.querySelectorAll('.square')];
 	for(const square of squares) {
-		square.classList.remove('black');
-		square.style.opacity = '';
+		square.style.background = '';
+		defaultColor = 1;
 	}
 });
 
-btnChangeGridSize.addEventListener('click', ()=>{
-	numberOfSquares = prompt('Insira um numero inteiro de 1 a 100');
-	render();
+btnRainbow.addEventListener('click', ()=>{
+	defaultColor = 2;
 });
 
+
+
+btnChangeGridSize.addEventListener('click', ()=>{
+	resize();
+});
+
+function parserInput(input){
+	if (input == null)
+		return 1;
+	const value = parseInt(input);
+	if(value > 0 && value < 101)
+		return 1;
+	return 0;
+}
 
 function cleanChild(){
 	if (gridContainer.childElementCount == 0)
@@ -27,34 +41,45 @@ function cleanChild(){
 		gridContainer.removeChild(child);
 }
 
-function putOnGridSquare(squares){
-	for(const square of squares) {
-		gridContainer.appendChild(square);
+function putColor(element) {
+	if (defaultColor == 1)
+	{
+		element.style.background = 'black';
+		return ;
 	}
+	let r = Math.floor(Math.random() * 255);
+	let g = Math.floor(Math.random() * 255);
+	let b = Math.floor(Math.random() * 255);
+	element.style.background = `rgb(${r}, ${g}, ${b})`;
 }
 
-function createSquares(){
-	const squares = [];
-	const sizeOfSquare = (SIZEGRID / parseFloat(numberOfSquares)).toFixed(2);
-	for(let i = 0; i < parseInt(numberOfSquares) * parseInt(numberOfSquares); i++) {
+function createSquares(input){
+	const sizeOfSquare = (SIZEGRID / parseFloat(input)).toFixed(2);
+	for(let i = 0; i < parseInt(input) * parseInt(input); i++) {
 		let element = document.createElement('div');
 		element.classList.add('square');
 		element.style.width = sizeOfSquare+'px';
 		element.style.height = sizeOfSquare+'px';
 		element.addEventListener('click', ()=>{
-			element.classList.add('black');
+			putColor(element);
 		});
-		squares.splice(i, 0, element);
+		gridContainer.appendChild(element);
 	}
-	return (squares);
 }
 
-function render(){
+function render(input){
+	if (input == null)
+		return ;
 	cleanChild();
-	gridContainer.style.width = SIZEGRID.toString()+'px';
-	const squares = createSquares();
-	putOnGridSquare(squares);
+	createSquares(input);
 }
 
-numberOfSquares = prompt('Insira um numero inteiro de 1 a 100');
-render();
+function resize() {
+	let input = '';
+	while(!parserInput(input))
+		input = prompt('Insira um numero inteiro de 1 a 100');
+	render(input);
+}
+
+gridContainer.style.width = SIZEGRID.toString()+'px';
+render(10);
